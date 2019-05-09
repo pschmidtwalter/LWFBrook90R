f0901840f1838dd818d5640934d6146cd3f1f381
Paul Schmidt-Walter <paul.schmidt-walter@nw-fva.de>
2019-05-09 21:49:32 +0200

    trying to remove warnings from CHECk 1 error resists in examples mrunLWFB90, cant reproduce

--- R/ptf_hamken_ff.R
+++ R/ptf_hamken_ff.R
@@ -25,7 +25,7 @@ hydpar_forestfloor_hamken <- function(n=1) {
   if (length(n) > 1) {
     warning("Only the first element of the supplied vector will be used." )
   }
-  out <- LWFBrook90R:::hydpar_forestfloor[rep(1,n[1]),]
+  out <- hydpar_forestfloor[rep(1,n[1]),]
   out$alpha <- out$alpha*100
   out
 }
