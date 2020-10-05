fe69a1f04e9e6c491e9753cdb35de1a7c7de5e22
Paul Schmidt-Walter <paul.schmidt-walter@nw-fva.de>
2020-10-05 15:47:32 +0200

    added tests for SWAT and BELO

--- tests/testthat/test-output_vs_b90exe.R
+++ tests/testthat/test-output_vs_b90exe.R
@@ -1,5 +1,6 @@
 skip("Will fail as long as the source codes differ!")
 library(LWFBrook90R)
+
 # ---- load test data ---------------------------------------------------------------
 # b90.exe output produced using 'data-raw/data_for_testing/b90exe_output/create_b90exe_refdat.R'
 load("data-raw/data_for_testing/b90exe_output/exe_output.rda")
@@ -35,23 +36,25 @@ test_that("inputs are equal", {
 # ------------------------------------------------------------------------------
 # Tests
 
+tolerance_mm <- 0.01
+
 # ----- ABOV-tests-----------------------------------------------------------
 
 # columns to test
 nms <- names(b90res$ABOVDAY.ASC) #[-which(names(b90res$BUDGDAY.ASC) %in% c("evap"))]
 
 test_that("values in ABOVDAY are equal", {
-  expect_equal(b90res$ABOVDAY.ASC[,nms, with = F], exe_res$ABOVDAY.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$ABOVDAY.ASC[,nms, with = F], exe_res$ABOVDAY.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 nms <- nms[!nms %in% c("da", "doy")]
 test_that("values in ABOVMON.ASC are equal", {
-  expect_equal(b90res$ABOVMON.ASC[,nms, with = F], exe_res$ABOVMON.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$ABOVMON.ASC[,nms, with = F], exe_res$ABOVMON.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 nms <- nms[nms != "mo"]
 test_that("values in ABOVANN.ASC are equal", {
-  expect_equal(b90res$ABOVANN.ASC[,nms, with = F], exe_res$ABOVANN.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$ABOVANN.ASC[,nms, with = F], exe_res$ABOVANN.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 # ----- BUDGDAY-tests-----------------------------------------------------------
@@ -62,27 +65,23 @@ b90res$BUDGANN.ASC[, intrs := intr+ints]
 b90res$BUDGDAY.ASC[, c("intr", "ints") := NULL]
 b90res$BUDGMON.ASC[, c("intr", "ints") := NULL]
 b90res$BUDGANN.ASC[, c("intr", "ints") := NULL]
-setnames(exe_res$BUDGANN.ASC, "intr+s", "intrs")
-setnames(exe_res$BUDGMON.ASC, "intr+s", "intrs")
-setnames(exe_res$BUDGDAY.ASC, "intr+s", "intrs")
+data.table::setnames(exe_res$BUDGANN.ASC, "intr+s", "intrs")
+data.table::setnames(exe_res$BUDGMON.ASC, "intr+s", "intrs")
+data.table::setnames(exe_res$BUDGDAY.ASC, "intr+s", "intrs")
 exe_res$BUDGDAY.ASC$mesfl <- NULL
 exe_res$BUDGMON.ASC$mesfl <- NULL
 exe_res$BUDGANN.ASC$mesfl <- NULL
 
-# columns to test
-nms <- names(b90res$BUDGDAY.ASC) #[-which(names(b90res$BUDGDAY.ASC) %in% c("evap"))]
-
 test_that("values in BUDGDAY are equal", {
-  expect_equal(b90res$BUDGDAY.ASC[,nms, with = F], exe_res$BUDGDAY.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$BUDGDAY.ASC, exe_res$BUDGDAY.ASC, tolerance = tolerance_mm)
 })
 
 test_that("values in BUDGMON are equal", {
-  expect_equal(b90res$BUDGMON.ASC, exe_res$BUDGMON.ASC, tolerance = 0.001)
+  expect_equal(b90res$BUDGMON.ASC, exe_res$BUDGMON.ASC, tolerance = tolerance_mm)
 })
 
 test_that("values in BUDGANN are equal", {
-  expect_equal(b90res$BUDGANN.ASC[,nms, with = F], exe_res$BUDGANN.ASC[,nms, with = F], tolerance = 0.001)
-
+  expect_equal(b90res$BUDGANN.ASC, exe_res$BUDGANN.ASC, tolerance = tolerance_mm)
 })
 
 # ----- EVAP-tests-----------------------------------------------------------
@@ -91,54 +90,96 @@ test_that("values in BUDGANN are equal", {
 nms <- names(b90res$EVAPDAY.ASC) #[-which(names(b90res$BUDGDAY.ASC) %in% c("evap"))]
 
 test_that("values in EVAPDAY.ASC are equal", {
-  expect_equal(b90res$EVAPDAY.ASC[,nms, with = F], exe_res$EVAPDAY.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$EVAPDAY.ASC[,nms, with = F], exe_res$EVAPDAY.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 nms <- nms[!nms %in% c("da", "doy")]
 test_that("values in EVAPMON.ASC are equal", {
-  expect_equal(b90res$EVAPMON.ASC[,nms, with = F], exe_res$EVAPMON.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$EVAPMON.ASC[,nms, with = F], exe_res$EVAPMON.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 nms <- nms[nms != "mo"]
 test_that("values in EVAPANN.ASC are equal", {
-  expect_equal(b90res$EVAPANN.ASC[,nms, with = F], exe_res$EVAPANN.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$EVAPANN.ASC[,nms, with = F], exe_res$EVAPANN.ASC[,nms, with = F], tolerance = tolerance_mm)
 })
 
 # ----- BELO-tests-----------------------------------------------------------
 exe_res$BELOANN.ASC <- exe_res$BELOANN.ASC[!is.na(yr),]
 
-# column to test
-nms <- names(b90res$BELODAY.ASC) #[-which(names(b90res$BUDGDAY.ASC) %in% c("evap"))]
-
 test_that("values in BELODAY.ASC are equal", {
-  expect_equal(b90res$BELODAY.ASC[,nms, with = F], exe_res$BELODAY.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$BELODAY.ASC, exe_res$BELODAY.ASC, tolerance = tolerance_mm)
 })
 
-nms <- nms[!nms %in% c("da", "doy")]
 test_that("values in BELOMON.ASC are equal", {
-  expect_equal(b90res$BELOMON.ASC[,nms, with = F], exe_res$BELOMON.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$BELOMON.ASC, exe_res$BELOMON.ASC, tolerance = tolerance_mm)
 })
 
-nms <- nms[nms != "mo"]
 test_that("values in BELOANN.ASC are equal", {
-  expect_equal(b90res$BELOANN.ASC[,nms, with = F], exe_res$BELOANN.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$BELOANN.ASC, exe_res$BELOANN.ASC, tolerance = tolerance_mm)
 })
 
 
-# ----- MISC-tests-----------------------------------------------------------
+# ----- MISC-tests -----------------------------------------------------------
 exe_res[c("MISCANN.ASC","MISCMON.ASC", "EPVPANN.ASC")] <- NULL
 b90res[c("MISCANN.ASC","MISCMON.ASC", "EPVPANN.ASC")] <- NULL
 exe_res$MISCDAY.ASC$awat40 <- NULL
 
-# column to test
-nms <- names(b90res$MISCDAY.ASC) #[-which(names(b90res$BUDGDAY.ASC) %in% c("evap"))]
-
 test_that("values in MISCDAY.ASC are equal", {
-  expect_equal(b90res$MISCDAY.ASC[,nms, with = F], exe_res$MISCDAY.ASC[,nms, with = F], tolerance = 0.001)
+  expect_equal(b90res$MISCDAY.ASC, exe_res$MISCDAY.ASC, tolerance = 0.001)
+})
+
+
+# ----- SWAT-tests -----------------------------------------------------------
+exe_res$SWATDAY.ASC$temperature <- NULL
+exe_res$SWATMON.ASC$temperature <- NULL
+exe_res$SWATANN.ASC$temperature <- NULL
+exe_res$SWATANN.ASC <- exe_res$SWATANN.ASC[!is.na(yr),]
+
+swat_wide <-  extract_layer_output(b90res$SWATDAY.ASC)
+exe_swat_wide <-  extract_layer_output(exe_res$SWATDAY.ASC)
+
+test_that("values in SWATDAY.ASC are equal", {
+  expect_equal(swat_wide, exe_swat_wide, tolerance = 0.001)
+})
+
+swat_wide <-  extract_layer_output(b90res$SWATMON.ASC)
+exe_swat_wide <-  extract_layer_output(exe_res$SWATMON.ASC)
+
+test_that("values in SWATMON.ASC are equal", {
+  expect_equal(swat_wide, exe_swat_wide, tolerance = 0.001)
+})
+
+swat_wide <-  extract_layer_output(b90res$SWATANN.ASC)
+exe_swat_wide <-  extract_layer_output(exe_res$SWATANN.ASC)
+
+test_that("values in SWATANN.ASC are equal", {
+  expect_equal(swat_wide, exe_swat_wide, tolerance = 0.001)
+})
+
+# ----- BELO-tests -----------------------------------------------------------
+exe_res$BELOANN.ASC <- exe_res$BELOANN.ASC[!is.na(yr),]
+
+belo_wide <-  extract_layer_output(b90res$BELODAY.ASC)
+exe_belo_wide <-  extract_layer_output(exe_res$BELODAY.ASC)
+
+test_that("values in BELODAY.ASC are equal", {
+  expect_equal(belo_wide, exe_belo_wide, tolerance = 0.001)
+})
+
+belo_wide <-  extract_layer_output(b90res$BELODAY.ASC)
+exe_belo_wide <-  extract_layer_output(exe_res$BELODAY.ASC)
+test_that("values in BELOMON.ASC are equal", {
+  expect_equal(belo_wide, exe_belo_wide, tolerance = 0.001)
+})
+
+belo_wide <-  extract_layer_output(b90res$BELODAY.ASC)
+exe_belo_wide <-  extract_layer_output(exe_res$BELODAY.ASC)
+test_that("values in BELOANN.ASC are equal", {
+  expect_equal(belo_wide, exe_belo_wide, tolerance = 0.001)
 })
 
 # clean up
-#rm(list = ls())
+rm(list = ls())
 
 
 
