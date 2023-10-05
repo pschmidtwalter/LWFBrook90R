584416ed6338cd940b0cbf53754f6b5fbf6ca7c6
Paul Schmidt-Walter <paul.schmidt-walter@dwd.de>
2023-10-05 07:53:20 +0000

    added manual tests outside testthat

---
+++ tests_compare_actual_vs_previous/create_testdata.R
@@ -0,0 +1,21 @@
+# This file creates and saves simulation results of the actual version ', in
+# order to test them for differences of the next version.
+library(LWFBrook90R)
+library(data.table)
+
+# Set up the input data
+data("slb1_soil")
+data("slb1_meteo")
+
+# create model input
+opts <- set_optionsLWFB90(startdate = as.Date("2002-01-01"), enddate = as.Date("2003-12-31"))
+parms <- set_paramLWFB90()
+soil <- cbind(slb1_soil, hydpar_wessolek_tab(texture = slb1_soil$texture))
+setDT(slb1_meteo)
+meteo <- slb1_meteo[which(dates >= opts$startdate
+                          & dates <= opts$enddate),]
+
+res <- run_LWFB90(options_b90 = opts, param_b90 = parms, climate = meteo, soil =soil)
+
+saveRDS(res, file = "inst/testdata/simresults.rds")
+saveRDS(meteo, file = "inst/testdata/meteo.rds")
---
+++ tests_compare_actual_vs_previous/test-compare_results_actual_vs_previous.R
@@ -0,0 +1,29 @@
+library(LWFBrook90R)
+library(data.table)
+
+# Set up the input data
+data("slb1_soil")
+data("slb1_meteo")
+
+# create model input
+opts <- set_optionsLWFB90(startdate = as.Date("2002-01-01"), enddate = as.Date("2003-12-31"))
+parms <- set_paramLWFB90()
+soil <- cbind(slb1_soil, hydpar_wessolek_tab(texture = slb1_soil$texture))
+setDT(slb1_meteo)
+meteo <- slb1_meteo[which(dates >= opts$startdate
+                          & dates <= opts$enddate),]
+
+res_actual <- run_LWFB90(options_b90 = opts, param_b90 = parms, climate = meteo, soil =soil)
+
+meteo <- readRDS(system.file("testdata/meteo.rds", package = "LWFBrook90R"))
+res_previous <- readRDS(system.file("testdata/simresults.rds", package = "LWFBrook90R"))
+
+test_that("sim-outputs of actual are equal to previous", {
+  expect_equal(res_actual$output, res_previous$output)
+  expect_equal(res_actual$layer_output, res_previous$layer_output)
+})
+
+test_that("sim-inputs of actual are equal to previous", {
+  expect_equal(res_actual$model_input$param_b90, res_previous$model_input$param_b90)
+  expect_equal(res_actual$model_input$standprop_daily, res_previous$model_input$standprop_daily)
+})
