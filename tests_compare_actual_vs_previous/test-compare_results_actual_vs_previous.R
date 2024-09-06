1b18249513f6f6f3e7c4f92a9a24f1ce297b69c7
--replace-all <paul.schmidt-walter@dwd.de>
2024-09-06 07:26:38 +0200

    test

--- tests_compare_actual_vs_previous/test-compare_results_actual_vs_previous.R
+++ tests_compare_actual_vs_previous/test-compare_results_actual_vs_previous.R
@@ -2,24 +2,15 @@ library(LWFBrook90R)
 library(data.table)
 
 # Set up the input data
-data("slb1_soil")
-data("slb1_meteo")
-
-# create model input
-opts <- set_optionsLWFB90(startdate = as.Date("2002-01-01"), enddate = as.Date("2003-12-31"))
-parms <- set_paramLWFB90()
-soil <- cbind(slb1_soil, hydpar_wessolek_tab(texture = slb1_soil$texture))
-setDT(slb1_meteo)
-meteo <- slb1_meteo[which(dates >= opts$startdate
-                          & dates <= opts$enddate),]
-
-res_actual <- run_LWFB90(options_b90 = opts, param_b90 = parms, climate = meteo, soil =soil)
-
-meteo <- readRDS(system.file("testdata/meteo.rds", package = "LWFBrook90R"))
-res_previous <- readRDS(system.file("testdata/simresults.rds", package = "LWFBrook90R"))
+meteo <- readRDS("tests_compare_actual_vs_previous/meteo.rds")
+res_previous <- readRDS("tests_compare_actual_vs_previous/simresults.rds")
+model_input <- res_previous$model_input
+res_actual <- run_LWFB90(options_b90 = model_input$options_b90,
+                         param_b90 = model_input$param_b90,
+                         climate = meteo)
 
 test_that("sim-outputs of actual are equal to previous", {
-  expect_equal(res_actual$output, res_previous$output)
+  expect_equal(res_actual$output, res_previous$output, tolerance = 0.001)
   expect_equal(res_actual$layer_output, res_previous$layer_output)
 })
 
